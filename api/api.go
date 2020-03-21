724863790dd7f580cf8fd5d6af14fb19b5fdef40
mingchengzack <chengmingjtt123@gmail.com>
2020-03-20 21:20:51 -0400

    added err checking for subrouter

--- api/api.go
+++ api/api.go
@@ -29,8 +29,10 @@ func (api *API) AddSubRouter(path string) {
 
 // SetSubRoute sets a route a subrouter
 func (api *API) SetSubRoute(subroute, path, method string, f http.HandlerFunc) {
-	subrouter := api.Subrouters[subroute]
-	subrouter.HandleFunc(path, f).Methods(method)
+	if _, ok := api.Subrouters[subroute]; !ok {
+		api.AddSubRouter(subroute)
+	}
+	api.Subrouters[subroute].HandleFunc(path, f).Methods(method)
 }
 
 // SetRoute sets a specific route
@@ -40,26 +42,34 @@ func (api *API) SetRoute(path, method string, f http.HandlerFunc) {
 
 // SubGet wraps the api for subrouter GET method
 func (api *API) SubGet(subroute, path string, f http.HandlerFunc) {
-	subrouter := api.Subrouters[subroute]
-	subrouter.HandleFunc(path, f).Methods("GET")
+	if _, ok := api.Subrouters[subroute]; !ok {
+		api.AddSubRouter(subroute)
+	}
+	api.Subrouters[subroute].HandleFunc(path, f).Methods("GET")
 }
 
 // SubPost wraps the api for subrouter POST method
 func (api *API) SubPost(subroute, path string, f http.HandlerFunc) {
-	subrouter := api.Subrouters[subroute]
-	subrouter.HandleFunc(path, f).Methods("POST")
+	if _, ok := api.Subrouters[subroute]; !ok {
+		api.AddSubRouter(subroute)
+	}
+	api.Subrouters[subroute].HandleFunc(path, f).Methods("POST")
 }
 
 // SubPut wraps the api for subrouter PUT method
 func (api *API) SubPut(subroute, path string, f http.HandlerFunc) {
-	subrouter := api.Subrouters[subroute]
-	subrouter.HandleFunc(path, f).Methods("PUT")
+	if _, ok := api.Subrouters[subroute]; !ok {
+		api.AddSubRouter(subroute)
+	}
+	api.Subrouters[subroute].HandleFunc(path, f).Methods("PUT")
 }
 
 // SubDelete wraps the api for subrouter DELETE method
 func (api *API) SubDelete(subroute, path string, f http.HandlerFunc) {
-	subrouter := api.Subrouters[subroute]
-	subrouter.HandleFunc(path, f).Methods("DELETE")
+	if _, ok := api.Subrouters[subroute]; !ok {
+		api.AddSubRouter(subroute)
+	}
+	api.Subrouters[subroute].HandleFunc(path, f).Methods("DELETE")
 }
 
 // Get wraps the api for GET method
--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/mingchengzack/REST-API/api"
 )
 
-func get(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "get"}`))
-}
-
 func main() {
 	api := api.NewAPI()
-	api.Get("/", get)
 	api.Run(":8080")
 }
